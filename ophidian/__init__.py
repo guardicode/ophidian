79e9e1451f1f08a278fe7b7151e4a614273c5a13
Mike Salvatore <mike.s.salvatore@gmail.com>
2023-11-22 08:43:15 -0500

    Add deprecation warning on import

--- ophidian/__init__.py
+++ ophidian/__init__.py
@@ -1,1 +1,9 @@
+from warnings import warn
+
 from .di_container import DIContainer, UnresolvableDependencyError
+
+warn(
+    "The `ophidian-di` package has been deprecated. Please use `ophidian` instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
